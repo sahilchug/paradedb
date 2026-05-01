967e156c8c88156ba2dc76ec5833d3436893a959
James Sewell <james.sewell@gmail.com>
2026-05-02 06:11:33 +1200

    feat: explicitly set columnar codec types for new indexes (#4951)
    Use tantivy's new codec selection mechanics to explicitly configure
    `[Bitpacked, BlockwiseLinear]` as the codec types for u64-based column
    serialization. These codec types are persisted in index metadata and
    used for initial index creation, REINDEX, subsequent segment merges, and
    in memory segments.
    
    This is in preparation for the `BlockwiseLinearV2` addition.
    
    **DO NOT MERGE UNTIL `Cargo.toml` is repointed at a main commit.**
    
    ---------
    
    Signed-off-by: James Sewell <james.sewell@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- nix/pg_search.nix
+++ nix/pg_search.nix
@@ -65,7 +65,7 @@ buildPgrxExtension (finalAttrs: {
   # If maintainers forget to do so, Nix will throw an error message that begins
   # like this and then provides the correct new hash:
   # error: hash mismatch in fixed-output derivation '...'
-  cargoHash = "sha256-xKCoYYJaKgIfK5CCH3u13J9b21otB8Qjbz+RQXKP1qM=";
+  cargoHash = "sha256-Dexyng9xtPrhGGf6xk983TwXW7LA5wziz+B/I7f6yWs=";
 
   inherit cargo-pgrx postgresql;
 
